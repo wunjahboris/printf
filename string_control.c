1d3aed6ad8d332b201388a99d1682bcbbb85d9d0
wunjahboris <83704385+wunjahboris@users.noreply.github.com>
2022-10-21 02:55:19 +0100

    string control

---
+++ string_control.c
@@ -0,0 +1,56 @@
+#include "main.h"
+
+/**
+ * print_reversed - Calls a function to reverse strings
+ * @arg: Argument passed to the function
+ * Return: The amount of characters printed
+ */
+int print_reversed(va_list arg)
+{
+	int len;
+	char *str;
+	char *ptr;
+
+	str = va_arg(arg, char *);
+	if (str == NULL)
+		return (-1);
+	ptr = rev_string(str);
+	if (ptr == NULL)
+		return (-1);
+	for (len = 0; ptr[len] != '\0'; len++)
+		_pchar(ptr[len]);
+	free(ptr);
+	return (len);
+}
+
+/**
+ * rot13 - Converts string to rot13
+ * @list: string to convert
+ * Return: converted string
+ */
+int rot13(va_list list)
+{
+	int i;
+	int x;
+	char *str;
+	char s[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char u[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	str = va_arg(list, char *);
+	if (str == NULL)
+		return (-1);
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (x = 0; x <= 52; x++)
+		{
+			if (str[i] == s[x])
+			{
+				_pchar(u[x]);
+				break;
+			}
+		}
+		if (x == 53)
+			_pchar(str[i]);
+	}
+	return (i);
+}
